f249e66402bc242383963886f1fdb221340b8130
gfdgd_xi <3025613752@qq.com>
2023-01-26 12:39:51 +0800

    更新2

--- update.py
+++ update.py
@@ -15,7 +15,7 @@ html = f"""<!DOCTYPE html>
     <h1>Wine 列表（来自 Wine 运行器）</h1>
     <p>点击下方链接下载编译好的 7z 包</p>
     <h3>推荐在 Wine 运行器内安装</h3>
-    <p><img src='images/截图_installwine_20230123094608.png'></p>
+    <p style='overflow: auto;'><img src='images/截图_installwine_20230123094608.png'></p>
     <hr/>
     <h3>更新时间：{datetime.datetime.now().year}年{datetime.datetime.now().month}月{datetime.datetime.now().day}日 {datetime.datetime.now().hour}:{datetime.datetime.now().minute}:{datetime.datetime.now().second}</h3>
 """
@@ -27,7 +27,7 @@ for i in lists:
 html += f"""    <p><a href='https://gitee.com/gfdgd-xi-org/deep-wine-runner/stargazers'><img src='https://gitee.com/gfdgd-xi-org/deep-wine-runner/badge/star.svg?theme=dark' alt='star'></img></a><a href='https://gitee.com/gfdgd-xi-org/deep-wine-runner/members'><img src='https://gitee.com/gfdgd-xi-org/deep-wine-runner/badge/fork.svg?theme=dark' alt='fork'></img></a></p>
     <hr/>
     <h2>Wine 运行器</h2>
-    <p><img src='https://gitee.com/gfdgd-xi-org/deep-wine-runner/widgets/widget_card.svg?colors=eae9d7,2e2f29,272822,484a45,eae9d7,747571'></p>
+    <p style='overflow: auto;'><img src='https://gitee.com/gfdgd-xi-org/deep-wine-runner/widgets/widget_card.svg?colors=eae9d7,2e2f29,272822,484a45,eae9d7,747571'></p>
     <hr/>
     <h1 id="copyright">©2020~{datetime.datetime.now().year} gfdgd xi、为什么您不喜欢熊出没和阿布呢</h1>
 </body>
